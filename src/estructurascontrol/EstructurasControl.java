7a110c05c4655fcb6eddc738154264aaa8a5ab0c
Moses <mmelchorm@miumg.edu.gt>
2024-04-11 16:36:58 -0600

    estructura inicial del proyecto

---
+++ src/estructurascontrol/EstructurasControl.java
@@ -0,0 +1,16 @@
+package estructurascontrol;
+
+/**
+ *
+ * @author desarrollador02
+ */
+public class EstructurasControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
